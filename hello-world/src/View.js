56381e6507cc1885b72c94f8f9191a333d543b8b
travisjn <tdizzletrav@gmail.com>
2017-03-25 01:26:34 -0700

    did some stuff

--- hello-world/src/View.js
+++ hello-world/src/View.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './App.js';
+import jssha from 'jssha';
 
 export class Display extends React.Component{
   constructor(props) {
     super(props);
-  }
-
-  getInitialState() {
-    console.log(arguments);
   }
 
   render() {
@@ -24,25 +21,44 @@ class ButtonFetchPrice extends React.Component {
   constructor(props) {
     super(props);
 
+
     this.state = {
+      sign: '',//sha512('169175e9298894d4da1138d127eac9cf8785c8625ac984b730ea03d6758e6440cd990fea8ff4937a908da6d7b222140b4fee41768b35bdc646987e87227e3b8c', 'command=returnBalances'),
       ethPrice: null,
+      queryParams: 'command=returnBalances',
       divStyle: {
         width: 100,
         height: 50
-      }
+      },
+      hmac: ''
     }
+
+    var shaObj = new jssha("SHA-512", "TEXT");
+    shaObj.setHMACKey("169175e9298894d4da1138d127eac9cf8785c8625ac984b730ea03d6758e6440cd990fea8ff4937a908da6d7b222140b4fee41768b35bdc646987e87227e3b8c", "TEXT");
+    shaObj.update(this.state.queryParams);
+    this.state.hmac = shaObj.getHMAC("HEX");
+
+
+    //console.log(sha512('169175e9298894d4da1138d127eac9cf8785c8625ac984b730ea03d6758e6440cd990fea8ff4937a908da6d7b222140b4fee41768b35bdc646987e87227e3b8ccommand=returnBalances'));
 
      this.handleClick = this.handleClick.bind(this);
   }
 
+  componentDidMount() {
+    //this.setState({sign: this.state.sign + sha512(this.state.queryParams)});
+  }
+
   handleClick() {
-    fetch('https://poloniex.com/public?command=returnTicker', {
+    console.log(this.state.hmac);
+    fetch('https://poloniex.com/tradingApi?' + this.state.queryParams, {
+      mode: 'no-cors',
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
         'Key': 'UBCYRORF-Y5EVNXIJ-90IXSP1X-AWQRCL0W',
-        'Sign': '169175e9298894d4da1138d127eac9cf8785c8625ac984b730ea03d6758e6440cd990fea8ff4937a908da6d7b222140b4fee41768b35bdc646987e87227e3b8c'
+        'Sign': this.state.hmac
       }
     })
     .then((response) => response.json())
